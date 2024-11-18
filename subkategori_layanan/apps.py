66b664c1d5f2717ec1dd29b8c200e815ec66f263
fakhrihabb <fakhrihabb@gmail.com>
2024-11-18 16:03:54 +0700

    load dummy data homepage

--- subkategori_layanan/apps.py
+++ subkategori_layanan/apps.py
@@ -4,3 +4,14 @@ from django.apps import AppConfig
 class SubkategoriLayananConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'subkategori_layanan'
+
+    def ready(self):
+        from .views import load_dummy_data
+        from django.http import HttpRequest
+
+        request = HttpRequest()
+        try:
+            load_dummy_data(request)
+            print("Dummy data loaded successfully.")
+        except Exception as e:
+            print(f"Error loading dummy data: {e}")
