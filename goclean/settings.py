be4a4c7a009312f05012d8e9153e4e500a88fec8
fakhrihabb <fakhrihabb@gmail.com>
2024-12-29 21:07:02 +0700

    bismillah part 33

--- goclean/settings.py
+++ goclean/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL="/media/"
 
