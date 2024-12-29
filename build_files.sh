b509b2cbc711685078264c5e54483ea0f0c7aa8d
Fakhri Habibi <68675332+fakhrihabb@users.noreply.github.com>
2024-12-29 20:15:02 +0700

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 #!/bin/bash
-python3 -m ensurepip --upgrade
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
+pip3.9 install -r requirements.txt
+python3.9 manage.py collectstatic --noinput
